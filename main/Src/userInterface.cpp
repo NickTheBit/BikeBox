41f2e33ded67190a8677e55a137165954ba1c4af
NickTheBit <nikosgklu@gmail.com>
2022-04-07 21:15:54 +0200

    Removed broken module

--- main/Src/userInterface.cpp
+++ main/Src/userInterface.cpp
@@ -8,13 +8,16 @@
 #include <esp_system.h>
 #include <esp_log.h>
 #include <UserInterface.hh>
+#include <lvgl/lvgl.h>
 
 extern "C" void UITask(void * parameter) {
 
+	lv_init();
     ESP_LOGI(TAG_Gui, "Gui task initialized");
 
     // User Interface loop
     while (1) {
+
         vTaskDelay(10);
     }
 }

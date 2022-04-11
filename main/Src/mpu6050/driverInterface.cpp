5f0b34676fb1ced17744cc60d4e11e77b6a64cd1
NickTheBit <nikosgklu@gmail.com>
2022-04-11 21:58:56 +0200

    Sensor configuration, now able to measure

--- main/Src/mpu6050/driverInterface.cpp
+++ main/Src/mpu6050/driverInterface.cpp
@@ -3,22 +3,16 @@
 //
 
 #include <mpu6050/driverInterface.h>
-#include <driver/i2c.h>
 #include "mpu6050/i2c_interface.h"
 
-#ifndef ESP32_I2C_IF_H
-#define ESP32_I2C_IF_H
-
-
-class ESP32_I2C_IF : public I2C_Interface
-{
-public:
 /**
 * @brief  I2C peripheral initialization method.
 * @param  clock I2C clock frequency (default 100 kHz)
 * @retval i2c_status_t
 */
-i2c_status_t Init_I2C(i2c_clockspeed_t clock = i2c_clockspeed_t::CLK_100KHz) override {
+i2c_status_t ESP32_I2C_IF::Init_I2C(i2c_clockspeed_t clock) {
+	master_conf.master.clk_speed = (uint32_t) clock;
+	i2c_param_config(I2C_Master_Port, &master_conf);
 	esp_err_t status = i2c_driver_install(I2C_Master_Port, I2C_MODE_MASTER, 16, 16, 0);
 	if (status != ESP_OK) {
 		return I2C_STATUS_ERROR;
@@ -34,7 +28,7 @@ i2c_status_t Init_I2C(i2c_clockspeed_t clock = i2c_clockspeed_t::CLK_100KHz) ove
 * @param  status Pointer for operation status
 * @retval uint8_t Read register value
 */
-uint8_t ReadRegister(uint8_t slaveAddress, uint8_t regAddress, i2c_status_t *status) override {
+uint8_t ESP32_I2C_IF::ReadRegister(uint8_t slaveAddress, uint8_t regAddress, i2c_status_t *status) {
 	uint8_t readBuffer;
 	esp_err_t err = i2c_master_read_from_device(I2C_Master_Port, slaveAddress, &readBuffer, sizeof(uint8_t), I2C_Timeout_ms);
 
@@ -61,7 +55,7 @@ uint8_t ReadRegister(uint8_t slaveAddress, uint8_t regAddress, i2c_status_t *sta
 * @param  data Data to be written
 * @retval i2c_status_t
 */
-i2c_status_t WriteRegister(uint8_t slaveAddress, uint8_t regAddress, uint8_t data) override {
+i2c_status_t ESP32_I2C_IF::WriteRegister(uint8_t slaveAddress, uint8_t regAddress, uint8_t data) {
 	esp_err_t err = i2c_master_write_to_device(I2C_Master_Port, slaveAddress, &data, sizeof(uint8_t), I2C_Timeout_ms);
 	if (err != ESP_OK) {
 		return I2C_STATUS_ERROR;
@@ -74,13 +68,6 @@ i2c_status_t WriteRegister(uint8_t slaveAddress, uint8_t regAddress, uint8_t dat
 * @param  none
 * @retval none
 */
-~ESP32_I2C_IF() override {
+ESP32_I2C_IF::~ESP32_I2C_IF() {
 	i2c_driver_delete(I2C_Master_Port);
 }
-private:
-const uint8_t I2C_Master_Port = 0;
-const uint16_t I2C_Timeout_ms = 100;
-
-};
-
-#endif
--- main/Src/recorder.cpp
+++ main/Src/recorder.cpp
@@ -7,7 +7,102 @@
 #include <recorder.hh>
 #include <esp_log.h>
 
-extern "C" void recorderTask(void * parameter) {
-    ESP_LOGI(TAG_Recorder, "Recording task initiated");
+#include <mpu6050/mpu6050.h>
+#include <mpu6050/i2c_interface.h>
+#include <mpu6050/driverInterface.h>
 
+extern "C" void recorderTask(void * parameter) {
+	ESP_LOGI(TAG_Recorder, "Recording task initiated");
+
+	// Initializing i2cInterface interface and sensors
+	ESP32_I2C_IF *i2cInterface = new ESP32_I2C_IF();
+	if (i2cInterface->Init_I2C(i2c_clockspeed_t::CLK_400KHz) != I2C_STATUS_SUCCESS) {
+		ESP_LOGW(TAG_Recorder,"I2C initialization failed!\n");
+		delete i2cInterface;
+		esp_restart();
+	}
+
+	MPU6050_Driver::MPU6050 sensor(i2cInterface);
+	if(sensor.ResetSensor() != I2C_STATUS_SUCCESS) {
+		ESP_LOGW(TAG_Recorder,"Sensor reset failed!\n");
+		esp_restart();
+	}
+	ESP_LOGI(TAG_Recorder,"Sensor reset completed!\n");
+	/* Simple safety delay after sensor reset */
+	vTaskDelay(100);
+
+	/* Wakeup sensor and set full scale ranges */
+	if(sensor.InitializeSensor(MPU6050_Driver::Gyro_FS_t::FS_1000_DPS, MPU6050_Driver::Accel_FS_t::FS_8G) != I2C_STATUS_SUCCESS)
+	{
+		ESP_LOGW(TAG_Recorder,"Sensor initialization failed!\n");
+		esp_restart();
+	}
+	ESP_LOGI(TAG_Recorder,"Sensor initialization completed!\n");
+
+	/* Auto-Calibrate gyroscope registers to target value 0 (default) */
+	if(sensor.Calibrate_Gyro_Registers() != I2C_STATUS_SUCCESS)
+	{
+		ESP_LOGW(TAG_Recorder,"Gyro calibration failed!\n");
+		esp_restart();
+	}
+
+	/* Auto-Calibrate accelerometer registers to target values by default:
+	 * X = 0 MG, Y = 0 MG, Z = 1 MG */
+	if(sensor.Calibrate_Accel_Registers() != I2C_STATUS_SUCCESS)
+	{
+		ESP_LOGW(TAG_Recorder,"Accel calibration failed!\n");
+		esp_restart();
+	}
+
+	/* set digital low pass to default value
+	 * (just to show the feature it already has default value in startup) */
+	if(sensor.SetSensor_DLPF_Config(MPU6050_Driver::DLPF_t::BW_260Hz) != I2C_STATUS_SUCCESS) {
+		ESP_LOGW(TAG_Recorder,"DLPF configuration failed!\n");
+		esp_restart();
+	}
+
+	/* set sample rate divider to default value
+	 * (just to show the feature it already has default value in startup) */
+	i2c_status_t error = I2C_STATUS_NONE;
+	if(sensor.SetGyro_SampleRateDivider(0) != I2C_STATUS_SUCCESS) {
+		ESP_LOGW(TAG_Recorder,"Sample rate config failed!\n");
+		esp_restart();
+	}
+	ESP_LOGI(TAG_Recorder,"Sample rate reading Succeeded!\n");
+
+	float currentSampleRateHz = sensor.GetSensor_CurrentSampleRate_Hz(&error);
+	if(error != I2C_STATUS_SUCCESS) {
+		ESP_LOGW(TAG_Recorder,"Sample rate reading failed!\n");
+		esp_restart();
+	}
+	ESP_LOGI(TAG_Recorder,"Sample rate reading Succeeded!\n");
+
+	ESP_LOGW(TAG_Recorder,"Sensor sample rate: %.2f Hz \n", currentSampleRateHz);
+	ESP_LOGW(TAG_Recorder,"Sensor configuration completed!\n");
+
+	/* Read sensor conversion registers for test! */
+	uint8_t totalRes = I2C_STATUS_SUCCESS;
+	i2c_status_t currentRes = I2C_STATUS_SUCCESS;
+	while(totalRes == I2C_STATUS_SUCCESS)
+	{
+		ESP_LOGW(TAG_Recorder,"Acc_X: %d ", sensor.GetAccel_X_Raw(&currentRes));
+		totalRes |= currentRes;
+		ESP_LOGW(TAG_Recorder,"Acc_Y: %d ", sensor.GetAccel_Y_Raw(&currentRes));
+		totalRes |= currentRes;
+		ESP_LOGW(TAG_Recorder,"Acc_Z: %d ", sensor.GetAccel_Z_Raw(&currentRes));
+		totalRes |= currentRes;
+		ESP_LOGW(TAG_Recorder,"Temp: %.2f ", sensor.GetTemperature_Celcius(&currentRes));
+		totalRes |= currentRes;
+		ESP_LOGW(TAG_Recorder,"Gyro_X: %d ", sensor.GetGyro_X_Raw(&currentRes));
+		totalRes |= currentRes;
+		ESP_LOGW(TAG_Recorder,"Gyro_Y: %d ", sensor.GetGyro_Y_Raw(&currentRes));
+		totalRes |= currentRes;
+		ESP_LOGW(TAG_Recorder,"Gyro_Z: %d \n", sensor.GetGyro_Z_Raw(&currentRes));
+		totalRes |= currentRes;
+		vTaskDelay(50 );
+	}
+
+	ESP_LOGW(TAG_Recorder,"Sensor read error! Program terminated!\n");
+	esp_restart();
 }
+
